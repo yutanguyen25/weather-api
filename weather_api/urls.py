ac0c729a49ca86146ec8af624a023adb3661f70c
yutanguyen <yuta.nguyen25@gmail.com>
2022-01-13 21:39:52 +0700

    First API

---
+++ weather_api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from weather_api import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('description', views.DescriptionViewset)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
